0640a4e975cd78d4e2b68f99668f6abb8ed51e66
dev <ronojit.dev20151@gmail.com>
2025-12-18 13:00:09 +0530

    daily_work_18-12-2025

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::post('/pay/process', [DemoPaymentController::class, 'processPayment']);
 Route::post('/bank/submit', [DemoPaymentController::class, 'bankSubmit']);
 Route::get('/payment-success', [DemoPaymentController::class, 'successPage']);
 
-Route::post('/payu/success', [DemoPaymentController::class, 'successPage'])->name('payu.success');
-Route::post('/payu/failure', [DemoPaymentController::class, 'failurePage'])->name('payu.failure');
+Route::post('payu/success', [DemoPaymentController::class, 'successPage'])->name('payu.success');
+Route::post('payu/failure', [DemoPaymentController::class, 'failurePage'])->name('payu.failure');
 // Route::get('/details', function () {
 //     return Inertia::render('details'); // details.jsx
 // });
